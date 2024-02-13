1562adac6ce990e54bbbc53a6a55dda924d87e55
U_s H_a <ushausama2@outlook.com>
2024-02-13 20:13:36 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDay1
+{
+    class Program
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            foreach(int i in nums)
+            {
+                Console.WriteLine($"Enter the number = {i}");
+            }
+            return nums;
+        }
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
